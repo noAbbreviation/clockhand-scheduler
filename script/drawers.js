f7710d289eee83ce224be99c9360db91a7c463a1
Jabez Villamarzo <math2btrue@gmail.com>
2023-08-19 20:08:38 +0800

    Add function params for draw_text

--- script/drawers.js
+++ script/drawers.js
@@ -15,3 +15,17 @@ function draw_rect(
     draw_ctx.fillStyle = fill_style;
     draw_ctx.fillRect(dimensions.pos_x, dimensions.pos_y, dimensions.width, dimensions.height);
 }
+
+function draw_text(
+    draw_ctx,
+    style = {
+        font: "20px serif",
+        textAlign: "center",
+    },
+    colors = {
+        fillStyle: "rgb(255,255,255)",
+        strokeStyle: "rgb(0,0,0)",
+    }
+) {
+
+}
