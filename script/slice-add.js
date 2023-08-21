e973708df49754008784fbd2f5eb6ef322b20e19
Jabez Villamarzo <math2btrue@gmail.com>
2023-08-21 22:52:07 +0800

    Add binders to form inputs on slice-add

--- script/slice-add.js
+++ script/slice-add.js
@@ -1,12 +1,24 @@
 function bind_slice_add_button() {
     const form = document.querySelector(".popups #slice-add-form");
-
     form.context = {};
-    const form_store = form.context;
 
+    const form_store = form.context;
     const time_start = form.querySelector("#time_start");
+    const time_end = form.querySelector("#time_end");
+    const color = form.querySelector("#slice_color");
+
     time_start.addEventListener("input", (event) => {
         form_store.time_start = event.target.value;
         console.log(form.context.time_start);
+    });
+
+    time_end.addEventListener("input", (event) => {
+        form_store.time_end = event.target.value;
+        console.log(form.context.time_start);
+    });
+
+    color.addEventListener("input", (event) => {
+        form_store.slice_color = event.target.value;
+        console.log(form.context.slice_color);
     });
 }
