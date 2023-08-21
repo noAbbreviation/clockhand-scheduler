caa2278ab5c4e17ddf3b0e57b206c4ced0ced84f
Jabez Villamarzo <math2btrue@gmail.com>
2023-08-21 22:10:58 +0800

    Refactor some code into render_main_canvas

--- script/main.js
+++ script/main.js
@@ -1,31 +1,37 @@
 let draw_ctx;
 
 window.addEventListener("DOMContentLoaded", () => {
+    render_main_canvas();
+
+    const options = document.querySelectorAll(".content button");
+    for (const option of options) {
+        option.addEventListener("click", bind_option_buttons);
+    }
+});
+
+function bind_option_buttons() {
+    render_starting_submenu_canvas();
+}
+
+function render_main_canvas() {
     const main_canvas = document.querySelector("#main-canvas");
     main_canvas.style["width"] = "100%";
     main_canvas.style["height"] = "100%";
-
+    
     if (main_canvas.getContext) {
         draw_ctx = main_canvas.getContext("2d");
 
         apply_defaults(draw_ctx);
         draw_starting_clock(draw_ctx);
     }
-
-    const options = document.querySelectorAll(".content button");
-    for (const option of options) {
-        option.addEventListener("click", bind_options_buttons);
-    }
-});
-
-function bind_options_buttons() {
-    render_starting_submenu_canvas();
 }
 
 function render_starting_submenu_canvas() {
     const sub_canvases = document.querySelectorAll("#submenu-canvas");
     for (const sub_canvas of sub_canvases) {
         const sub_ctx = sub_canvas.getContext("2d");
+        sub_canvas.style["width"] = "100%";
+        sub_canvas.style["height"] = "100%";
 
         draw_starting_clock(sub_ctx);
     }
