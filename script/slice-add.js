109bd7381f606f345014afd28e4af9dbe0d731e9
Jabez Villamarzo <math2btrue@gmail.com>
2023-08-22 15:36:25 +0800

    Refactor form_store to be inside childs

--- script/slice-add.js
+++ script/slice-add.js
@@ -5,6 +5,8 @@ function bind_slice_add_form() {
     const form_inputs = form.querySelectorAll("input");
     for (const input of form_inputs) {
         input.addEventListener("input", input_change_slice_add);
+
+        input.form_store = form.context;
     }
 }
 
@@ -12,11 +14,6 @@ function input_change_slice_add(event) {
     const element = event.target;
     const new_value = element.value;
 
-    let form_store = element.parentElement; 
-    while (form_store.nodeName !== undefined && form_store.nodeName.toLowerCase() !== "form") {
-        form_store = form_store.parentElement;
-    }
-
-    form_store.context[element.id] = new_value;
-    console.log(form_store.context);
+    element.form_store[element.id] = new_value;
+    console.log(element.form_store[element.id]);
 }
