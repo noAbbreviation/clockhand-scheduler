3ced0691361c829af14ecc9df773e767ccec80fa
Jabez Villamarzo <math2btrue@gmail.com>
2023-08-24 22:33:22 +0800

    Add slice prop to input on input-select-edit

--- script/slice-edit.js
+++ script/slice-edit.js
@@ -64,8 +64,19 @@ function input_select_edit(event) {
 
         element.slice_index = selected_value;
         slices[element.slice_index].selected = true;
+
+        const slice = slices[element.slice_index];
+        const form = document.querySelector("#slice-edit-form");
+        for (const prop in slice) {
+            const input = form.querySelector(`input[id="${prop}"]`);
+            input.value = slice[prop];
+        }
+        
     } else if (slice_index !== -1) {
         slices[slice_index].selected = false;
+
+        inputs_field.disabled = true;
+        // todo: add reset here too?
     }
     
     const edit_object = slices[element.slice_index];
@@ -73,7 +84,6 @@ function input_select_edit(event) {
     for (const prop in edit_object) {
         form_store[prop] = edit_object[prop]; 
     }
-    console.log(form_store);
 
     draw_clock_slices(draw_ctx, slices);
 }
@@ -82,11 +92,12 @@ function input_slice_edit(event) {
     const element = event.target;
     const new_value = element.value;
 
-    element.form_store[element.id] = new_value;
+    const form_store = element.form_store;
+    form_store[element.id] = new_value;
 
     const draw_ctx = element.draw_ctx;
-    const form_store = element.form_store;
     const slices = get_globals().slices;
     
     draw_clock_slices(draw_ctx, slices);
+    draw_new_slice(draw_ctx, form_store);
 }
