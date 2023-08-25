6fd32d6c62f06a65065b8b038c04f84919823837
Jabez Villamarzo <math2btrue@gmail.com>
2023-08-25 22:16:24 +0800

    Move globals init to globals.js

--- script/main.js
+++ script/main.js
@@ -12,51 +12,6 @@ window.addEventListener("DOMContentLoaded", () => {
 
 function init_globals() {
     const main_container = document.querySelector(".main-container");
-    const globals = {};
-    
-    globals.main_canvas = {};
-
-    globals.clock_circle_style = {
-        center: {
-            pos_x: 250,
-            pos_y: 250,
-        },
-        circle_style: {
-            lineWidth: "10",
-            strokeStyle: "rgb(175, 175, 175)",
-        },
-        inner_dot_style: {
-            lineWidth: "10",
-            strokeStyle: "black",
-        },
-        outer_dot_style: {
-            lineWidth: "15",
-            strokeStyle: defaults.fillStyle,
-        },
-        circle_radius: 230,
-    };
-
-    globals.clock_texts_style = {
-        center: globals.clock_circle_style.center,
-        text_radius: globals.clock_circle_style.circle_radius * 88 * 0.01,
-        clock_text_style: {
-            fillStyle: "rgb(49, 49, 49)",
-        },
-    };
-
-    globals.clock_slices_style = {
-        inner_stroke: {
-            lineWidth: "3",
-        },
-        outer_stroke: {
-            lineWidth: "6",
-            strokeStyle: "rgb(253, 255, 240)",
-            fillStyle: "rgb(253, 253, 253)",
-        }
-    };
-
-    // test slices, pls remove at prod
-    globals.slices = [];
     
     main_container.context = {};
     main_container.context = globals;
